d8344ea20474b7ac7350119e5ca7b5749458dc6e
Maria Shady <maria.shady@yahoo.com>
2019-09-25 16:57:35 -0400

    friends.js objects set up

---
+++ app/data/friends.js
@@ -0,0 +1,41 @@
+let friends = [
+
+    {
+        name: "Chandler",
+        photo: "",
+        scores: [5, 2, 3, 4, 4, 5, 1, 2, 5, 5]
+    },
+
+    {
+        name: "Joey",
+        photo: "",
+        scores: [3, 1, 5, 2, 3, 5, 5, 2, 4, 5]
+
+    },
+
+    {
+        name: "Monica",
+        photo: "",
+        scores: [4, 5, 2, 1, 1, 3, 5, 4, 3, 1]
+    },
+
+    {
+        name: "Phoebe",
+        photo: "",
+        scores: [3, 2, 5, 5, 4, 1, 3, 3, 5, 5]
+
+    },
+
+    {
+        name: "Rachel",
+        photo: "",
+        scores: [2, 4, 4, 3, 2, 5, 4, 3, 4, 4 ]
+    ,}
+
+    {
+        name: "Ross",
+        photo: "",
+        scores: [1, 5, 5, 3, 4, 2, 1, 4, 2, 5]
+    }
+]
+  
